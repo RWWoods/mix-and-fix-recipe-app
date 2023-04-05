8e7a5b5e6c50180b60ab0320e46e0667b1ec6f0a
myroman-wuLtCL <myroman@protonmail.com>
2023-04-05 12:11:46 -0400

    updated js and html

---
+++ assets/js/food.js
@@ -0,0 +1,47 @@
+var dishNameInput = document.querySelector("#dish-name-input")
+
+var dishNameButton = document.querySelector("#dish-name-button")
+
+var dishIngredientInput = document.querySelector("#dish-ingredient-input")
+
+var dishIngredientButton = document.querySelector("#dish-ingredient-button")
+
+var results = document.getElementById("results")
+
+function dishName(data) {
+
+    console.log(data)
+}
+
+function dishNameFetch(name) {
+    fetch("https://themealdb.com/api/json/v1/1/search.php?s=" + name).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        dishName(data);
+    });
+};
+
+
+dishNameButton.addEventListener("click", function (event){
+    dishNameFetch(dishNameInput.value)
+});
+
+
+
+
+function dishIngredient(data) {
+    console.log(data)
+}
+
+function dishIngredientFetch(ingredient) {
+    fetch("https://themealdb.com/api/json/v1/1/filter.php?i==" + ingredient).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        dishIngredient(data);
+    });
+};
+
+
+dishIngredientButton.addEventListener("click", function (event) {
+    dishIngredientFetch(dishIngredientInput.value)
+});
