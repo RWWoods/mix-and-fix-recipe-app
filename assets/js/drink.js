1ed1c883208314ac561ee3f3245020edf680f009
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-05 10:21:06 -0400

    added fetch response to drink page for ingredients

--- assets/js/drink.js
+++ assets/js/drink.js
@@ -2,13 +2,17 @@ var cocktailNameInput= document.querySelector("#cocktail-name-input")
 
 var cocktailNameButton = document.querySelector("#cocktail-name-button")
 
+var cocktailIngredientInput = document.querySelector("#cocktail-ingredient-input")
+
+var cocktailIngredientButton = document.querySelector("#cocktail-ingredient-button")
+
 
 function cocktailName(data) {
     console.log(data)
 }
 
-function cocktailNameFetch () {
-    fetch("https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita").then(function(response){
+function cocktailNameFetch (name) {
+    fetch("https://thecocktaildb.com/api/json/v1/1/search.php?s=" + name).then(function(response){
       return response.json()
     }).then(function(data){
       cocktailName(data);
@@ -18,4 +22,24 @@ function cocktailNameFetch () {
 
   cocktailNameButton.addEventListener("click", function(event){
     cocktailNameFetch(cocktailNameInput.value)
+  });
+
+
+
+
+  function cocktailIngredient(data) {
+    console.log(data)
+} 
+
+function cocktailIngredientFetch (ingredient) {
+    fetch("https://thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient).then(function(response){
+      return response.json()
+    }).then(function(data){
+      cocktailIngredient(data);
+    });
+  };
+
+
+  cocktailIngredientButton.addEventListener("click", function(event){
+    cocktailIngredientFetch(cocktailIngredientInput.value)
   });
