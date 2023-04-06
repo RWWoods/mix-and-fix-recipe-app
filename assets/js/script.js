49f9665a87a5ff1b2fd28da739f8897fd01d8fbe
Kiggles <ridgewwoods@gmail.com>
2023-04-06 14:48:42 -0400

    added functionality to random meal button in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,55 @@
 var randomCocktailButton = document.querySelector("#luckyCocktail");
 
+var randomFoodButton = document.querySelector("#luckyMeal");
+
+var mealTile = document.querySelector("#food-column");
+
 var drinkTile = document.querySelector("#drink-column");
+
+
+function randomMeal() {
+    var requestUrl = "https://themealdb.com/api/json/v1/1/random.php"
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            luckyMeal(data);
+        })
+
+}
+
+function luckyMeal(data) {
+    var luckyMeal = data.meals[0]
+    console.log(data);
+
+
+    var ingredients = [luckyMeal.strIngredient1, luckyMeal.strIngredient2, luckyMeal.strIngredient3,
+        luckyMeal.strIngredient4, luckyMeal.strIngredient5, luckyMeal.strIngredient6, luckyMeal.strIngredient7,
+        luckyMeal.strIngredient8, luckyMeal.strIngredient9, luckyMeal.strIngredient10, luckyMeal.strIngredient11,
+    luckyMeal.strIngredient12, luckyMeal.strIngredient13, luckyMeal.strIngredient14, luckyMeal.strIngredient15]
+
+    ingredients = ingredients.filter(function (element) {
+        return element != null
+    })
+
+    var measure = [luckyMeal.strMeasure1, luckyMeal.strMeasure2, luckyMeal.strMeasure3, luckyMeal.strMeasure4,
+        luckyMeal.strMeasure5, luckyMeal.strMeasure6, luckyMeal.strMeasure7, luckyMeal.strMeasure8, luckyMeal.strMeasure9,
+        luckyMeal.strMeasure10, luckyMeal.strMeasure11, luckyMeal.strMeasure12, luckyMeal.strMeasure13, luckyMeal.strMeasure14, luckyMeal.strMeasure15]
+
+    measure = measure.filter(function (element) {
+        return element != null
+    });
+
+    var instructions = luckyMeal.strInstructions;
+    var foodRecipe = "Meal Name: " + luckyMeal.strMeal + ". " + ingredients.join(", ") + ". " + measure.join(", ") + ". " + instructions;
+
+    mealTile.textContent = foodRecipe;
+
+}
+
+// randomMeal();
+
 
 function randomDrink() {
     var requestUrl = "https://thecocktaildb.com/api/json/v1/1/random.php"
@@ -53,6 +102,12 @@ function luckyCocktail(data) {
 randomCocktailButton.addEventListener("click", function (event) {
     event.stopImmediatePropagation();
     randomDrink();
+
+})
+
+randomFoodButton.addEventListener("click", function (event) {
+    event.stopImmediatePropagation();
+    randomMeal();
 
 })
 
