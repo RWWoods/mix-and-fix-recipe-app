ac71bb382dcab750a12a587af012e5a12c4f8ab0
Kiggles <ridgewwoods@gmail.com>
2023-04-06 10:20:14 -0400

    working on printing results of search to page

--- assets/js/drink.js
+++ assets/js/drink.js
@@ -1,3 +1,5 @@
+var APIkey = "1";
+
 var cocktailNameInput= document.querySelector("#cocktail-name-input")
 
 var cocktailNameButton = document.querySelector("#cocktail-name-button")
@@ -6,6 +8,9 @@ var cocktailIngredientInput = document.querySelector("#cocktail-ingredient-input
 
 var cocktailIngredientButton = document.querySelector("#cocktail-ingredient-button")
 
+var recipeList = document.querySelector('ul');
+
+console.log(recipeList);
 
 function cocktailName(data) {
     console.log(data)
@@ -43,3 +48,37 @@ function cocktailIngredientFetch (ingredient) {
   cocktailIngredientButton.addEventListener("click", function(event){
     cocktailIngredientFetch(cocktailIngredientInput.value)
   });
+
+
+  // fetch(requestUrl)
+  // .then(function (response) {
+  //   return response.json();
+  // })
+  // .then(function (data) {
+  //   for (var i = 0; i < data.length; i++) {
+  //     var listItem = document.createElement('li');
+  //     listItem.textContent = data[i].html_url;
+  //     repoList.appendChild(listItem);
+  //   }
+  // });
+
+
+  function getApi() {
+
+    var requestUrl = 'https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json(); })
+    .then(function(data) {
+      for (var i = 0; i < data.length; i++) {
+        var listItem = document.createElement('li');
+        
+        listItem.innerHTML = data[i].idDrink;
+        recipeList.appendChild(listItem);
+      }
+      console.log(data[i]);
+    })
+  }
+
+  cocktailNameButton.addEventListener('click', getApi);
